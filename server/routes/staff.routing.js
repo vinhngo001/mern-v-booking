4b27c3081d79a36d665169be34ad85dc34898812
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-23 13:51:45 +0700

    [api] - get all staffs

--- server/routes/staff.routing.js
+++ server/routes/staff.routing.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 
 
 //Load Staff Model
-const staff = require("../models/staff.model");
+const Staff = require("../models/staff.model");
 
 // CREATE / GET
 router.get("/create", async (req, res) => {
@@ -30,6 +30,33 @@ router.get("/create", async (req, res) => {
     } catch (error) {
         console.log(error);
         return res.status(500).json(error.message);
+    }
+});
+
+// LIST / GET
+router.get("/list", async (req, res, next) => {
+    const accessIdentity = await authHelper.getAccessToken(req.cookies, res);
+    if (accessIdentity) {
+        let parms = {
+            title: 'All Staff',
+            active: {
+                staff: true
+            },
+            user: accessIdentity.username
+        }
+
+        // Get all businesss
+        Staff.find({
+            boss: accessIdentity.email
+        }).then(staffs => {
+            parms.staffs = staffs
+            res.render("staffs/index", parms);
+        }).catch(err => {
+            console.error(err.message);
+            next(err);
+        })
+    } else {
+        res.redirect("/");
     }
 })
 
